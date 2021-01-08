86dacadbd657219de3a5954bad120d8d5061032b
Leticia Franco <leticiafrnc1@gmail.com>
2021-01-08 11:15:25 -0300

    primeiro commit

---
+++ js/function.js
@@ -0,0 +1,162 @@
+function valida() {
+			if (document.form1.sel.value=="") {
+
+				
+				 window.alert("Wrong answer, try again!");
+            document.form1.sel.focus();
+			} else window.alert("Congratulations!"); // caso eu queira colocar quando a resposta não estiver certa !
+		}
+
+		function valida2() {
+			if (document.form2.sel2.value=="") {
+
+				
+				 window.alert("Wrong answer, try again!");
+            document.form2.sel2.focus();
+			} else window.alert("Congratulations!"); // caso eu queira colocar quando a resposta não estiver certa !
+		}
+		function valida3() {
+			if (document.form3.sel3.value=="") {
+
+				
+				 window.alert("Wrong answer, try again!");
+            document.form3.sel3.focus();
+			} else window.alert("Congratulations!"); // caso eu queira colocar quando a resposta não estiver certa !
+		}
+		function valida4() {
+			if (document.form4.sel4.value=="") {
+
+				
+				 window.alert("Wrong answer, try again!");
+            document.form4.sel4.focus();
+			} else window.alert("Congratulations!"); // caso eu queira colocar quando a resposta não estiver certa !
+		}
+		function valida5() {
+			if (document.form5.sel5.value=="") {
+
+				
+				 window.alert("Wrong answer, try again!");
+            document.form5.sel5.focus();
+			} else window.alert("Congratulations!"); // caso eu queira colocar quando a resposta não estiver certa !
+		}
+		function valida6() {
+			if (document.form6.sel6.value=="") {
+
+				
+				 window.alert("Wrong answer, try again!");
+            document.form6.sel6.focus();
+			} else window.alert("Congratulations!"); // caso eu queira colocar quando a resposta não estiver certa !
+		}
+		function valida7() {
+			if (document.form7.sel7.value=="") {
+
+				
+				 window.alert("Wrong answer, try again!");
+            document.form7.sel7.focus();
+			} else window.alert("Congratulations!"); // caso eu queira colocar quando a resposta não estiver certa !
+		}
+		function valida8() {
+			if (document.form8.sel8.value=="") {
+
+				
+				 window.alert("Wrong answer, try again!");
+            document.form8.sel8.focus();
+			} else window.alert("Congratulations!"); 
+		
+		function valida9() {
+			if (document.form9.sel9.value=="") {
+
+				
+				 window.alert("Wrong answer, try again!");
+            document.form9.sel9.focus();
+			} else window.alert("Congratulations!"); // caso eu queira colocar quando a resposta não estiver certa !
+		}
+		function valida10() {
+			if (document.form10.sel0.value=="") {
+
+				
+				 window.alert("Wrong answer, try again!");
+            document.form10.sel0.focus();
+			} else window.alert("Congratulations!"); // caso eu queira colocar quando a resposta não estiver certa !
+		}
+		function valida11() {
+			if (document.form11.sel1.value=="") {
+
+				
+				 window.alert("Wrong answer, try again!");
+            document.form11.sel11.focus();
+			} else window.alert("Congratulations!"); // caso eu queira colocar quando a resposta não estiver certa !
+		}
+		function valida12() {
+			if (document.form12.sel12.value=="") {
+
+				
+				 window.alert("Wrong answer, try again!");
+            document.form12.sel12.focus();
+			} else window.alert("Congratulations!"); // caso eu queira colocar quando a resposta não estiver certa !
+		}
+		function valida13() {
+			if (document.form13.sel13.value=="") {
+
+				
+				 window.alert("Wrong answer, try again!");
+            document.form13.sel13.focus();
+			} else window.alert("Congratulations!"); // caso eu queira colocar quando a resposta não estiver certa !
+		}
+		function valida14() {
+			if (document.form14.sel14.value=="") {
+
+				
+				 window.alert("Wrong answer, try again!");
+            document.form14.sel14.focus();
+			} else window.alert("Congratulations!"); // caso eu queira colocar quando a resposta não estiver certa !
+		}
+		function valida15() {
+			if (document.form15.sel15.value=="") {
+
+				
+				 window.alert("Wrong answer, try again!");
+            document.form15.sel15.focus();
+			} else window.alert("Congratulations!"); // caso eu queira colocar quando a resposta não estiver certa !
+		}
+		function valida16() {
+			if (document.form16.sel16.value=="") {
+
+				
+				 window.alert("Wrong answer, try again!");
+            document.form16.sel16.focus();
+			} else window.alert("Congratulations!"); // caso eu queira colocar quando a resposta não estiver certa !
+		}
+		function valida17() {
+			if (document.form17.sel17.value=="") {
+
+				
+				 window.alert("Wrong answer, try again!");
+            document.form17.sel17.focus();
+			} else window.alert("Congratulations!"); // caso eu queira colocar quando a resposta não estiver certa !
+		}
+		function valida18() {
+			if (document.form18.sel18.value=="") {
+
+				
+				 window.alert("Wrong answer, try again!");
+            document.form18.sel18.focus();
+			} else window.alert("Congratulations!"); // caso eu queira colocar quando a resposta não estiver certa !
+		}
+		function valida19() {
+			if (document.form19.sel19.value=="") {
+
+				
+				 window.alert("Wrong answer, try again!");
+            document.form19.sel19.focus();
+			} else window.alert("Congratulations!"); // caso eu queira colocar quando a resposta não estiver certa !
+		}
+		function valida20() {
+			if (document.form20.sel20.value=="") {
+
+				
+				 window.alert("Wrong answer, try again!");
+            document.form20.sel20.focus();
+			} else window.alert("Congratulations!"); // caso eu queira colocar quando a resposta não estiver certa !
+		}
+	}
